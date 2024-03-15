e57903e2a0b5bfc71f061f5dae3235b9495a2458
aphelionxd <r.batista@aluno.ifsp.edu.br>
2024-03-15 12:31:00 -0300

    footer

--- WebSite/login/script.js
+++ WebSite/login/script.js
@@ -2,10 +2,6 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+registerBtn.addEventListener('click', () => { container.classList.add("active"); });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+loginBtn.addEventListener('click', () => { container.classList.remove("active"); });
