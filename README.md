bdcc7a6bddd24f837e55294880b9f84b1939863f
aphelionxd <r.batista@aluno.ifsp.edu.br>
2024-03-31 23:01:19 -0300

    finalizando

--- README.md
+++
@@ -1,7 +0,0 @@
-# Projeto Integrador 2°
-
-**Desenvolvedores:**
-*  Ricardo Oliveira Batista mailto:r.batista@aluno.ifsp.edu.br
-* Gabrielle Aparecida de Castro de Sousa mailto:gabrielle.castro@aluno.ifsp.edu.br
-* Natan Rafael de Lima da Cruz mailto:natan.cruz@aluno.ifsp.edu.br
-* Guilherme Bizerra de Souza mailto:g.bizerra@aluno.ifsp.edu.br
--- WebSite/login/script.js
+++ WebSite/login/script.js
@@ -5,3 +5,9 @@ const loginBtn = document.getElementById('login');
 registerBtn.addEventListener('click', () => { container.classList.add("active"); });
 
 loginBtn.addEventListener('click', () => { container.classList.remove("active"); });
+
+const backButton = document.getElementById('back-button');
+
+backButton.addEventListener('click', () => {
+  history.back();
+});
