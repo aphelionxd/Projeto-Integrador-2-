d8fe673b0e1066cbc22c7276e408b4ab912d7940
aphelionxd <r.batista@aluno.ifsp.edu.br>
2024-06-05 12:21:50 -0300

    edit index css

--- WebSite/js/script.js
+++ WebSite/js/script.js
@@ -9,3 +9,17 @@ images.forEach(img => {
         img.parentNode.querySelector('p').style.fontSize = '16px';
     });
 });
+
+
+//Mover elementos para dentro da página ao rolar o scroll
+$(document).ready(function() {
+    $('.animate__animated').each(function() {
+      var element = $(this);
+      var animation = element.data('animation');
+      element.waypoint(function() {
+        element.removeClass('invisible').addClass(animation);
+      }, {
+        offset: '90%'
+      });
+    });
+  });
